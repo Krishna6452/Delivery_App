52e66fb32c8a4e7b78259c1fe6cc1b6943d72464
Krishna6452 <krishnapriya6452@gmail.com>
2024-11-03 20:32:40 +0530

    Create App.jsx

---
+++ frontend/src/App.jsx
@@ -0,0 +1,16 @@
+function App() {
+
+  return (
+    <h3 className='text-6xl text-center font-bold min-h-screen grid place-content-center'>
+      Its a beginning
+      <img className="absolute top-2 left-2 w-1/12" src="https://hacktoberfest.com/_next/static/media/logo-hacktoberfest-11--green.dd2bc4ca.svg" alt="hack logo" />
+      <div className="bg-bg-color text-text-color">
+        <a className="text-xl bg-primary-color mt-5 text-white p-2 rounded"
+          href={"https://github.com/devvsakib/rapidroute"}
+        >Visit Repository</a>
+      </div>
+    </h3>
+  )
+}
+
+export default App
